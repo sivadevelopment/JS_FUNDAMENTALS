2a0b7a528986ba020c5018864813cda010d822f2
sivadevelopment <sivannarayanapeddireddi99@gmail.com>
2022-04-19 09:53:44 +0530

    chnages

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let arr = [5, 1, 3, 2, 6];
 // map Transform
 // Double every element in the array or tripple every element in the array
 
+/*
 let doubleArr = arr.map((item) => {
   return item * 2;
 });
@@ -49,3 +50,71 @@ let findMaxArr = arr.reduce((max, item) => {
 }, 0);
 console.log(sumOfArr);
 console.log(findMaxArr);
+*/
+
+let userArray = [
+  {
+    firstName: "srivalli",
+    lastName: "naga",
+    age: 16,
+  },
+  {
+    firstName: "siva",
+    lastName: "peddireddi",
+    age: 18,
+  },
+  {
+    firstName: "dattanash",
+    lastName: "naga",
+    age: 10,
+  },
+  {
+    firstName: "sudhir",
+    lastName: "venkata",
+    age: 16,
+  },
+  {
+    firstName: "manvitha",
+    lastName: "reddi",
+    age: 18,
+  },
+  {
+    firstName: "manish",
+    lastName: "reddi",
+    age: 10,
+  },
+];
+
+let firstnameLastnaeArr = userArray.map((item) => {
+  return item.firstName + " " + item.lastName;
+});
+
+console.log(firstnameLastnaeArr);
+
+let ageClaArr = userArray.reduce((arr, item) => {
+  if (arr[item.age]) {
+    arr[item.age] = ++arr[item.age];
+  } else {
+    arr[item.age] = 1;
+  }
+  return arr;
+}, {});
+
+console.log(ageClaArr);
+
+let filteredByAgeArr = userArray.filter((item) => {
+  return item.age > 15;
+});
+console.log(filteredByAgeArr);
+let filteredByAgeArrNames = filteredByAgeArr.map((item) => {
+  return item.firstName;
+});
+console.log(filteredByAgeArrNames);
+
+let filteredByAgeArrNamesReduce = userArray.reduce((arr, item) => {
+  if (item.age > 15) {
+    arr.push(item.firstName);
+  }
+  return arr;
+}, []);
+console.log(filteredByAgeArrNamesReduce);
